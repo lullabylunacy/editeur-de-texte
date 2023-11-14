ae20675c6cdd61d74c5b5e00363cca7d54508457
garciam <matteo.garcia@etu.umontpellier.fr>
2023-11-14 17:03:57 +0100

    feature : javadoc

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -1,5 +1,7 @@
 package fr.iut.editeur.document;
-
+/**
+ * gère les actions que chaque commande doit effectuer
+ */
 public class Document {
 
     private String texte;
@@ -19,11 +21,21 @@ public class Document {
     public void ajouter(String texte) {
         this.texte += texte;
     }
+    /**
+     * Description de la méthode
+     * @param start est l'indice du premier caractere à remplacer
+     * @param end est l'indice du der caractere à remplacer
+     */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
         texte = leftPart + remplacement + rightPart;
     }
+    /**
+     * Description de la méthode
+     * @param start est l'indice du premier caractere à mettre en majuscule
+     * @param end est l'indice du der caractere à mettre en majuscule
+     */
     public void majuscules(int start, int end) {
         remplacer(start,end,texte.substring(start,end).toUpperCase());
     }
